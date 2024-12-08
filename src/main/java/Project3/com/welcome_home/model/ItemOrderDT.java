53684845ecd6427b22712f9ec79c1132b17c08fd
swooshie <adityaajhaveri@gmail.com>
2024-12-07 20:48:07 -0500

    Problem 6 done.

---
+++ src/main/java/Project3/com/welcome_home/model/ItemOrderDT.java
@@ -0,0 +1,8 @@
+package Project3.com.welcome_home.model;
+
+public class ItemOrderDT {
+    public Integer ItemID;
+    public String client;
+    public String supervisor;
+    public String notes;
+}
